55abc6c49fefa0cb3cdc4035fd47ab8bb8dbd89d
Sean Harrison <sah@bookgenesis.com>
2020-07-03 11:32:19 -0500

    change / to use a JSON response via serde::Serialize

--- api/src/api.rs
+++ api/src/api.rs
@@ -1,5 +1,15 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{HttpResponse, Result};
+use serde::{Serialize};
 
-pub async fn index() -> impl Responder {
-    HttpResponse::Ok().body("{\"status\": 200, \"message\": \"Welcome to the Ark API\"}")
+#[derive(Serialize)]
+struct Message {
+    status: u16,
+    message: String,
+}
+
+pub async fn index() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json(Message {
+        status: 200,
+        message: "Welcome to the Ark API".to_string(),
+    }))
 }
