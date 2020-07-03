d3328b6bd1a80a1718f9236da0cb4ff246914e79
Sean Harrison <sah@bookgenesis.com>
2020-07-03 14:01:20 -0500

    /health – status of archive server, using `reqwest`

--- api/src/api/health.rs
+++ api/src/api/health.rs
@@ -1,6 +1,7 @@
 use crate::api;
 use actix_web::{HttpResponse, Result};
 use futures::join;
+use reqwest;
 use serde::Serialize;
 use std::{env, format, fs};
 
@@ -12,6 +13,7 @@ struct HealthStatus {
 }
 
 pub async fn index() -> Result<HttpResponse> {
+    // query the health of services in parallel (by using join!)
     let (archive_files_status, archive_server_status, postgresql_status) = join!(
         archive_files_health(),
         archive_server_health(),
@@ -19,11 +21,13 @@ pub async fn index() -> Result<HttpResponse> {
     );
 
     // return HealthStatus with the status of each service
-    Ok(HttpResponse::Ok().json(HealthStatus {
-        files: archive_files_status,
-        archive: archive_server_status,
-        postgres: postgresql_status,
-    }))
+    Ok(HttpResponse::Ok().json(
+        HealthStatus {
+            files: archive_files_status,
+            archive: archive_server_status,
+            postgres: postgresql_status,
+        }
+    ))
 }
 
 async fn archive_files_health() -> api::Message {
@@ -35,7 +39,7 @@ async fn archive_files_health() -> api::Message {
         Ok(archive_files) => match fs::metadata(&archive_files) {
             Ok(_) => api::Message {
                 status: 200,
-                message: "OK".to_string(),
+                message: "Ok".to_string(),
             },
             Err(_) => api::Message {
                 status: 404,
@@ -46,15 +50,27 @@ async fn archive_files_health() -> api::Message {
 }
 
 async fn archive_server_health() -> api::Message {
-    api::Message {
-        status: 501,
-        message: "NOT IMPLEMENTED".to_string(),
+    match env::var("ARCHIVE_SERVER") {
+        Err(err) => api::Message {
+            status: 404,
+            message: format!("Error: {}", &err).to_string(),
+        },
+        Ok(archive_server) => match reqwest::get(&archive_server).await {
+            Err(err) => api::Message {
+                status: 502,
+                message: format!("Error: {}", &err).to_string(),
+            },
+            Ok(_) => api::Message {
+                status: 200,
+                message: "Ok".to_string(),
+            }
+        }
     }
 }
 
 async fn postgresql_health() -> api::Message {
     api::Message {
         status: 501,
-        message: "NOT IMPLEMENTED".to_string(),
+        message: "Not Implemented".to_string(),
     }
 }
