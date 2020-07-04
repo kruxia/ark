2a127463324d912da5a239be5a1112e0f1df8212
Sean Harrison <sah@bookgenesis.com>
2020-07-04 14:43:40 -0500

    create get_database_pool() function to get that functionality

--- api/src/main.rs
+++ api/src/main.rs
@@ -9,23 +9,25 @@ pub struct AppState {
     database: PgPool,
 }
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    let mut listenfd = ListenFd::from_env();
-    let max_size: u32 = 5;
-    let database = PgPool::builder()
+async fn get_database_pool(max_size: u32) -> PgPool {
+    PgPool::builder()
         .max_size(max_size)
         .build(match &env::var("DATABASE_URL") {
             Ok(url) => url,
             Err(_) => "",
         })
         .await
-        .unwrap();
+        .unwrap()
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
+    let max_size: u32 = 5;
+    let database: PgPool = get_database_pool(max_size).await;
     let mut server = HttpServer::new(move || {
         App::new()
-            .data(AppState {
-                database: database.clone(),
-            })
+            .data(AppState { database: database.clone() })
             .route("/", web::get().to(api::index))
             .route("/health", web::get().to(api::health::index))
     });
