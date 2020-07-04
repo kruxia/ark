87704fba9505fd80b4cad415a8f1d1c71e3806f4
Sean Harrison <sah@bookgenesis.com>
2020-07-04 14:44:25 -0500

    in api health, change "postgres" to "database" — more general

--- api/src/api/health.rs
+++ api/src/api/health.rs
@@ -11,22 +11,22 @@ use std::{env, format, fs};
 struct HealthStatus {
     files: api::StatusMessage,
     archive: api::StatusMessage,
-    postgres: api::StatusMessage,
+    database: api::StatusMessage,
 }
 
 pub async fn index(_req: HttpRequest, app_state: web::Data<AppState>) -> Result<HttpResponse> {
     // query the health of services in parallel (by using join!)
-    let (archive_files_status, archive_server_status, postgresql_status) = futures::join!(
+    let (archive_files_status, archive_server_status, database_status) = futures::join!(
         archive_files_health(),
         archive_server_health(),
-        postgresql_health(app_state),
+        database_health(app_state),
     );
 
     // return HealthStatus with the status of each service
     Ok(HttpResponse::Ok().json(HealthStatus {
         files: archive_files_status,
         archive: archive_server_status,
-        postgres: postgresql_status,
+        database: database_status,
     }))
 }
 
@@ -34,16 +34,16 @@ async fn archive_files_health() -> api::StatusMessage {
     match env::var("ARCHIVE_FILES") {
         Err(_) => api::StatusMessage {
             status: 404,
-            message: "environment variable ARCHIVE_FILES does not exist".to_string(),
+            message: "environment variable ARCHIVE_FILES is not set".to_string(),
         },
         Ok(archive_files) => match fs::metadata(&archive_files) {
+            Err(_) => api::StatusMessage {
+                status: 502,
+                message: format!("ARCHIVE_FILES NOT FOUND: {}", &archive_files).to_string(),
+            },
             Ok(_) => api::StatusMessage {
                 status: 200,
                 message: "Ok".to_string(),
-            },
-            Err(_) => api::StatusMessage {
-                status: 404,
-                message: format!("ARCHIVE_FILES NOT FOUND: {}", &archive_files).to_string(),
             },
         },
     }
@@ -68,7 +68,7 @@ async fn archive_server_health() -> api::StatusMessage {
     }
 }
 
-async fn postgresql_health(app_state: web::Data<AppState>) -> api::StatusMessage {
+async fn database_health(app_state: web::Data<AppState>) -> api::StatusMessage {
     match sqlx::query("SELECT true")
         .execute(&app_state.database)
         .await
