f1ae556d33a86ac414ea1fdaaad3f300201e0ea5
Sean Harrison <sah@bookgenesis.com>
2020-07-04 16:13:31 -0500

    runtests.sh — ensure that the svn server and test database are availabe.

--- api/runtests.sh
+++ api/runtests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# make sure the supporting services are running
+docker-compose up -d
+
+# make sure the postgres test server exists
+psql -q -c "DROP DATABASE IF EXISTS ${DATABASE_NAME}_test;" ${DATABASE_URL}
+psql -q -c "CREATE DATABASE ${DATABASE_NAME}_test;" ${DATABASE_URL}
+
 # some of the tests change env::var values without mutex, so we can only use one thread
 # (TODO: Make these tests thread-safe.)
 
-cargo test $@ -- --test-threads=1
+DATABASE_URL="${DATABASE_URL}_test" cargo test $@ -- --test-threads=1
