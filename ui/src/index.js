a9a86c33e9eb725a274379dec810d99e2f43b1d4
Sean Harrison <sah@bookgenesis.com>
2020-07-15 11:06:17 -0500

    configure tailwind, webpack, and mithril

---
+++ ui/src/index.js
@@ -0,0 +1,4 @@
+import m from "mithril";
+m.render(document.getElementById("main"), 
+    <h1 class="font-mono">hello world</h1>
+);
---
+++ ui/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  important: true,
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    // fontFamily: {
+    //   display: ['Noto Sans', 'sans-serif'],
+    //   body: ['Noto Serif', 'serif'],
+    // },
+  },
+  variants: {},
+  plugins: [],
+}
