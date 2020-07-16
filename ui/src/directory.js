a056c60c2d7d07ee01a4162b08b31f6606beca1b
Sean Harrison <sah@bookgenesis.com>
2020-07-15 19:32:39 -0500

    DirectoryList with a little styling

--- ui/src/directory.js
+++ ui/src/directory.js
@@ -9,7 +9,6 @@ var Directory = {
             withCredentials: false,
         }).then(function (result) {
             Directory.data = result
-            m.render(Directory)
         })
     },
 }
@@ -17,33 +16,48 @@ var Directory = {
 var DirectoryList = {
     oninit: Directory.load,
     view: function () {
-        return Directory.data.info && Directory.data.files && (
-            <div>
-                <h1>{Directory.data.info.path.name}</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>name</th>
-                            <th>size</th>
-                            <th>last modified</th>
-                            <th>rev</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Directory.data.files.map((value, index) => {
-                            return (
-                                <tr key="{index}">
-                                    <td><a href={value.path.url}>{value.path.name}</a></td>
-                                    <td>{value.path.size}</td>
-                                    <td>{value.version.date}</td>
-                                    <td>{value.version.rev}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        ) || "loading..."
+        if (Directory.data.info && Directory.data.files) {
+            return (
+                <div>
+                    <table class="table-auto w-full">
+                        <thead>
+                            <tr>
+                                <th class="border-b px-3 py-2 text-left">name</th>
+                                <th class="border-b px-3 py-2 text-left">size</th>
+                                <th class="border-b px-3 py-2 text-left">last modified</th>
+                                <th class="border-b px-3 py-2 text-right">rev</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Directory.data.files.map((value, index) => {
+                                return (
+                                    <tr key="{index}">
+                                        <td class="border-b px-3 py-2 text-left">
+                                            <a href={value.path.url}>{value.path.name}</a>
+                                        </td>
+                                        <td class="border-b px-3 py-2 text-left">
+                                            {value.path.size}
+                                        </td>
+                                        <td class="border-b px-3 py-2 text-left">
+                                            {
+                                                value.version.date
+                                                    .replace(/\.\d+/, '').replace('T', ' ')
+                                                    .replace('+00:00', ' UTC')
+                                            }
+                                        </td>
+                                        <td class="border-b px-3 py-2 text-right">
+                                            {value.version.rev}
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            )
+        } else {
+            return "loading..."
+        }
     }
 }
 
