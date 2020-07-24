fc5dbed8905292975ab362e7dea189522cd11e2e
Sean Harrison <sah@bookgenesis.com>
2020-07-24 12:36:23 -0500

    ErrorView moved to error.js

--- ui/src/archives.js
+++ ui/src/archives.js
@@ -5,7 +5,8 @@ const {
     IconHistory, IconHistoryOff, IconSpinningCircles
 } = require('./icons')
 const { HistoryPanel } = require('./history')
-var { PATH, Breadcrumbs, PathLink } = require('./path')
+const { PATH, Breadcrumbs, PathLink } = require('./path')
+const { ErrorView } = require('./error')
 
 var ArchivePathView = {
     oninit: (vnode) => {
@@ -26,25 +27,6 @@ var ArchivePathView = {
             // nothing yet
             // return <div class="mx-2">loading...</div>
         }
-    }
-}
-
-var ErrorView = {
-    view: function () {
-        return (
-            <div class="leading-tight">
-                <div class="text-2xl font-light">
-                    <Breadcrumbs />
-                </div>
-                <h1 class="mx-2 mt-2 text-4xl font-black">{PATH.error.code}</h1>
-                <p class="mx-2 uppercase">{PATH.error.message}</p>
-                <div class="mx-2 my-2">
-                    {/* An HTTP status dog to brighten your day */}
-                    <img src={'https://httpstatusdogs.com/img/' + PATH.error.code + '.jpg'}
-                        alt={'HTTP ' + PATH.error.code + ' dog'} />
-                </div>
-            </div>
-        )
     }
 }
 
@@ -199,8 +181,8 @@ var FileView = {
                             PATH.mimetype.match(/(?:^(?:text|image)|(?:xml|pdf)$)/) ? (
                                 <FileIFrame />
                             ) : (
-                                <FileNoPreview />
-                            )
+                                    <FileNoPreview />
+                                )
                         }
                     </div>
                 </div>
