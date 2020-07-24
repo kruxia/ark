15499464aedc05c3cf67baf1ae89f83f5c644f14
Sean Harrison <sah@bookgenesis.com>
2020-07-24 12:13:23 -0500

    ErrorView moved to error.js

--- ui/src/archives.js
+++ ui/src/archives.js
@@ -6,7 +6,8 @@ const {
 } = require('./icons')
 const { HistoryPanel } = require('./history')
 const { Modal } = require('./modal')
-var { PATH, Breadcrumbs, PathLink } = require('./path')
+const { PATH, Breadcrumbs, PathLink } = require('./path')
+const { ErrorView } = require('./error')
 
 var ArchivePathView = {
     oninit: (vnode) => {
@@ -303,25 +304,6 @@ var FileNoPreview = {
     }
 }
 
-var ErrorView = {
-    view: function () {
-        return (
-            <div class="leading-tight">
-                <div class="text-2xl font-light">
-                    <Breadcrumbs />
-                </div>
-                <h1 class="mx-2 mt-2 text-4xl font-black">{PATH.error.code}</h1>
-                <p class="mx-2 uppercase">{PATH.error.message}</p>
-                <div class="mx-2 my-2">
-                    {/* An HTTP status dog to brighten your day */}
-                    <img src={'https://httpstatusdogs.com/img/' + PATH.error.code + '.jpg'}
-                        alt={'HTTP ' + PATH.error.code + ' dog'} />
-                </div>
-            </div>
-        )
-    }
-}
-
 // == DIRECTORY ACTIONS ==
 // (only on directories)
 
@@ -337,7 +319,6 @@ var ActionCreateFolder = {
         )
     },
     create: function (event) {
-        event.preventDefault();
         const folderName = window.prompt("Folder Name:", "")
         console.log(folderName)
         if (folderName) {
---
+++ ui/src/error.js
@@ -0,0 +1,22 @@
+const m = require('mithril')
+
+var ErrorView = {
+    view: function () {
+        return (
+            <div class="leading-tight">
+                <div class="text-2xl font-light">
+                    <Breadcrumbs />
+                </div>
+                <h1 class="mx-2 mt-2 text-4xl font-black">{PATH.error.code}</h1>
+                <p class="mx-2 uppercase">{PATH.error.message}</p>
+                <div class="mx-2 my-2">
+                    {/* An HTTP status dog to brighten your day */}
+                    <img src={'https://httpstatusdogs.com/img/' + PATH.error.code + '.jpg'}
+                        alt={'HTTP ' + PATH.error.code + ' dog'} />
+                </div>
+            </div>
+        )
+    }
+}
+
+module.exports = { ErrorView }
