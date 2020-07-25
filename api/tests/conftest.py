2c2db44bc4368fea2b1653eb48435b9c30caa316
Sean Harrison <sah@bookgenesis.com>
2020-07-25 06:29:17 -0500

    Starlette TestClient with app database connection. Woo! Now we can have a sane python testing environment.

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,50 +1,20 @@
+import asyncio
 import os
+import databases
 import pytest
-import requests
-import subprocess
 from starlette.testclient import TestClient
 from api.main import app
-
-# class TestClient:
-#     HOST = 'http://127.0.0.1'
-#     PORT = 8001
-
-#     def get(self, url, *args, **kwargs):
-#         return requests.get(f"{self.HOST}:{self.PORT}{url}", *args, **kwargs)
-
-#     def post(self, url, *args, **kwargs):
-#         return requests.post(f"{self.HOST}:{self.PORT}{url}", *args, **kwargs)
-
-#     def put(self, url, *args, **kwargs):
-#         return requests.put(f"{self.HOST}:{self.PORT}{url}", *args, **kwargs)
-
-#     def delete(self, url, *args, **kwargs):
-#         return requests.delete(f"{self.HOST}:{self.PORT}{url}", *args, **kwargs)
-
-
-# def launch_subprocess_app():
-#     cmd = (
-#         ['uvicorn']
-#         + ['--host', '0.0.0.0', '--port', f'{TestClient.PORT}']
-#         + ['api.main:app']
-#     )
-#     app = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,)
-#     line = app.stderr.readline()
-#     while b'Uvicorn running' not in line:
-#         line = app.stderr.readline()
-#     return app
 
 
 @pytest.fixture(scope="session")
 def setup():
-    # app = launch_subprocess_app()
     os.environ['ARK_TEST_PREFIX'] = '__ARK_TEST.'
+    app.database = databases.Database(os.getenv('DATABASE_URL'), force_rollback=True)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(app.database.connect())
     yield
-    # app.kill()
-    # app.communicate()  # wait for it to exit
 
 
 @pytest.fixture()
 def client(setup):
-    # return TestClient()
     return TestClient(app)
