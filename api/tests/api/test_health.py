910f086d2adb2938f199dcb839b21b15b6ef3ae9
Sean Harrison <sah@bookgenesis.com>
2020-07-25 05:17:07 -0500

    disable postgres

--- api/tests/api/test_health.py
+++ api/tests/api/test_health.py
@@ -5,6 +5,10 @@ def test_health_ok(client):
     response = client.get('/health')
     assert response.status_code == 200
     data = response.json()
-    for key in ['files', 'archive', 'database']:
+    for key in [
+        'files',
+        'archive',
+        # 'database'
+    ]:
         assert data[key]['code'] == 200
         assert data[key]['message'] == 'OK'
