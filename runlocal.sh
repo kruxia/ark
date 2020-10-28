99c4a69a61f061205201b505258e44659b0dd9b9
Sean Harrison <sah@bookgenesis.com>
2020-10-27 23:21:23 -0500

    move runlocal.sh to the main project level -- it's useful there.

--- runlocal.sh
+++ runlocal.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+CWD=$(pwd)
+
 docker-compose up -d api
 
+cd $(dirname $0)/ui
 mkdir -p dist 
 cp -R public/ dist/
 node_modules/.bin/tailwind build src/static/main.css -o dist/static/main.css 
@@ -10,3 +13,5 @@ npm run watch &
 npm run dev &
 
 docker-compose logs -f
+
+cd $CWD
