062227e93f1ed34cb27d2370137785e8354eccd6
Sean Harrison <sah@bookgenesis.com>
2020-11-04 03:52:53 -0600

    Merge postgres branch's conftest.py into the main one.

--- ark/tests/conftest.py
+++ ark/tests/conftest.py
@@ -1,4 +1,6 @@
+import asyncio
 import os
+import databases
 import pytest
 from starlette.testclient import TestClient
 from api.main import app
@@ -6,8 +8,19 @@ from api.main import app
 
 @pytest.fixture(scope="session")
 def setup():
+    # Rather than going to obscene lengths to create a separate archive server, we just
+    # create test archives with a given prefix to avoid name conflicts. We will publish
+    # '__ARK' as a reserved archive prefix.
     os.environ['ARK_TEST_PREFIX'] = '__ARK_TEST.'
-    yield
+    
+    # The app isn't running normally, so the database connection (pool) that we usually
+    # create during app startup (api.main.app_startup) has to be done here.
+    app.db = databases.Database(os.getenv('DATABASE_URL'), force_rollback=True)
+
+    # The Database.connect method is async, so it has to be done in the main event loop.
+    # (We use asyncio.get_event_loop() to get the main event loop shared by TestClient.)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(app.db.connect())
 
 
 @pytest.fixture()
