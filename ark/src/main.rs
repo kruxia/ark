2f9a21db91cf6047a2a03997d87f919b033b0799
Sean Harrison <sah@kruxia.com>
2024-03-15 23:35:28 -0500

    ark - rust backend initialized

---
+++ ark/src/main.rs
@@ -0,0 +1,50 @@
+use axum::{ routing::{ get, post }, http::StatusCode, Json, Router };
+use rand::RngCore;
+use serde::{ Deserialize, Serialize };
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new().route("/", get(root)).route("/users", post(create_user));
+
+    // run our app with hyper, listening globally on port 8000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: rand::thread_rng().next_u64(), // random id
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
