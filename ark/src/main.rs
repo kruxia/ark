620a7be157e16a94bacdbf58ab8fe4f181f448ee
Sean Harrison <sah@kruxia.com>
2024-03-16 16:39:11 -0500

    accounts - GET /accounts with UUID id

--- ark/src/main.rs
+++ ark/src/main.rs
@@ -1,6 +1,8 @@
-use axum::{ routing::{ get, post }, http::StatusCode, Json, Router };
-use rand::RngCore;
-use serde::{ Deserialize, Serialize };
+use axum::{http::StatusCode, routing::get, Json, Router};
+// use diesel::sql_types::{Jsonb, Uuid};
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
@@ -8,43 +10,39 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     // build our application with a route
-    let app = Router::new().route("/", get(root)).route("/users", post(create_user));
+    let app: Router = Router::new()
+        .route("/", get(home))
+        .route("/accounts", get(search_accounts));
 
     // run our app with hyper, listening globally on port 8000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener: TcpListener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
+async fn home() -> &'static str {
     "Hello, World!"
 }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>
-) -> (StatusCode, Json<User>) {
+async fn search_accounts(// this argument tells axum to parse the request body
+    // as JSON into a `SearchAccount` type
+    // Json(payload): Json<SearchAccount>,
+) -> (StatusCode, Json<Account>) {
     // insert your application logic here
-    let user = User {
-        id: rand::thread_rng().next_u64(), // random id
-        username: payload.username,
+    let account: Account = Account {
+        id: Uuid::new_v4(), // random id
+        title: "Foo Bar".to_string(),
     };
 
     // this will be converted into a JSON response
     // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
+    (StatusCode::OK, Json(account))
 }
 
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+// the input to our `account` handler
+#[derive(Deserialize, Serialize)]
+struct Account {
+    id: Uuid,
+    title: String,
+    // meta: Option<Json<Jsonb>>,
 }
